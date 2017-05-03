f3427be7b59efeb44e5eaccebf4e8c34b0f030da
Dexter <antoinelanco@gmail.com>
2017-05-03 11:17:45 +0200

    no message

--- src/Direction.java
+++ src/Direction.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 enum Direction {
-	NORTH(0, -1), SOUTH(0, 1), EAST(1, 0), WEST(-1, 0), NORTH_EAST(1, -1), NORTH_WEST(-1, -1), SOUTH_EAST(1,1), SOUTH_WEST(-1, 1);
+	NORTH(0, -1), SOUTH(0, 1), EAST(1, 0), WEST(-1, 0);
 
 	private static final List<Direction> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
 	private final int dI, dJ;
