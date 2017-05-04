47cf099795943f03597206d3cc64d1e97b39076d
Dexter <antoinelanco@gmail.com>
2017-05-04 16:56:56 +0200

    commit pre pull

--- src/Hero.java
+++ src/Hero.java
@@ -80,10 +80,9 @@ public class Hero extends Thread{
 	}
 	
 
-	public void paintHero(Graphics2D g2d, int scale) throws IOException {
+	public void paintHero(Graphics2D g2d, int scale)  {
 		//g2d.drawImage(image, this.cell.geti() * scale, this.cell.getj() * scale, scale, scale, null);
 
-		
 		if(!this.model.getState()){
 			g2d.setPaint(Color.YELLOW);
 		}else{
@@ -102,6 +101,8 @@ public class Hero extends Thread{
 		int[] xPoints = {(this.cell.geti()*scale)+(scale/2),(this.cell.geti()+1)*scale,(this.cell.geti()+1)*scale};
 		int[] yPoints = {(this.cell.getj()*scale)+(scale/2)+(scale/10),(this.cell.getj()*scale)+(3*scale/4)+(scale/10),(this.cell.getj()*scale)+(scale/4)+(scale/10)};
 		g2d.fillPolygon(xPoints, yPoints, 3);
+		
+
 	}
 	
 	public void run(){
@@ -133,6 +134,34 @@ public class Hero extends Thread{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+}
+
+class HeroMove extends Thread{
+	
+	private Graphics2D g2d;
+	private int scale;
+	private Cell cell;
+	private Model model;
+	
+	
+	public HeroMove(Graphics2D g2d, int scale, Cell cell, Model model) {
+		this.g2d = g2d;
+		this.scale = scale;
+		this.cell = cell;
+		this.model = model;
+	}
+
+	public void run(){
+		
+		int temp = this.scale;
+		for(int i=0;i<temp;i++){
+			
+
+			
+			this.scale++;
+
 		}
 	}
 }
